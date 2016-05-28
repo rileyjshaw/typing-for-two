e38968d85430b46d92beb708317ea0433f09df21
Riley Shaw <rileyjshaw@gmail.com>
2016-05-28 18:13:45 -0400

    Add score

--- Tutor.elm
+++ Tutor.elm
@@ -10,8 +10,9 @@ import Keyboard
 
 
 type alias Model =
-  { test : String
+  { sourceText : String
   , attempt : String
+  , score: Int
   }
 
 
@@ -22,24 +23,34 @@ type Msg
 t = "Some folks think that I'm just lazy, but the truth is that I'm really crazy"
 
 init : (Model, Cmd Msg)
-init = (Model t "", Cmd.none)
+init = (Model t "" 0, Cmd.none)
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
     Keypress code ->
-      ({ model | attempt = model.attempt ++ String.fromChar ( Char.fromCode code ) }, Cmd.none)
+      let
+        letter = String.fromChar ( Char.fromCode code )
+        len = String.length model.attempt
+      in
+        ({ model |
+          attempt = model.attempt ++ letter
+        , score = model.score + if letter == (String.slice len (len + 1) t) then 1 else 0
+        }, Cmd.none)
 
 
 view : Model -> Html Msg
 view model =
-  div []
-    ([ span [] (List.map2 (\a b -> if a == b
+  div [] (
+    [ span [] (List.map2 (\a b -> if a == b
         then span [style [ ("color", "#0f0") ]] [text (String.fromChar a)]
         else span [style [ ("color", "#f00") ]] [text (String.fromChar a)]
       )
-      (String.toList model.test) (String.toList model.attempt)
-    )] ++ [span [] [text (String.dropLeft (String.length model.attempt) t)]])
+      (String.toList model.sourceText) (String.toList model.attempt)
+    )]
+    ++ [span [] [text (String.dropLeft (String.length model.attempt) t)]]
+    ++ [span [] [text (toString model.score)]]
+  )
 
 
 subscriptions : Model -> Sub Msg
