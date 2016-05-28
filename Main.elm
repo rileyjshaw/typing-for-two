f799a09f66bd09d939be25228a91b5da419bef69
phil <uniphil@gmail.com>
2016-05-28 16:46:21 -0400

    add a monitor thing

--- Main.elm
+++ Main.elm
@@ -3,6 +3,7 @@ import Html.App as Html
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 import Tutor as Tutor
+import Monitor as Monitor
 
 main =
   Html.program
@@ -15,22 +16,26 @@ main =
 type alias Model =
   { leftTutor : Tutor.Model
   , rightTutor: Tutor.Model
+  , monitor: Monitor.Model
   }
 
 type Msg
   = LeftTutor Tutor.Msg
   | RightTutor Tutor.Msg
+  | MonitorMsg Monitor.Msg
 
 
 init : (Model, Cmd Msg)
 init =
   let
     (tutorModel, tutorCmd) = Tutor.init
+    (monitorModel, monitorCmd) = Monitor.init
   in
-    (Model tutorModel tutorModel, Cmd.batch
-      [ Cmd.map LeftTutor tutorCmd
-      , Cmd.map RightTutor tutorCmd
-      ])
+    (Model tutorModel tutorModel monitorModel,
+      Cmd.batch [ Cmd.map LeftTutor tutorCmd
+                , Cmd.map RightTutor tutorCmd
+                , Cmd.map MonitorMsg monitorCmd
+                ])
 
 
 update : Msg -> Model -> (Model, Cmd Msg)
@@ -38,24 +43,37 @@ update msg model =
   case msg of
     LeftTutor leftMsg ->
       let
-        (leftModel, leftCmd) =
+        (leftModel, cmd) =
             Tutor.update leftMsg model.leftTutor
       in
-        ({ model | leftTutor = leftModel }, Cmd.map LeftTutor leftCmd)
+        ({ model | leftTutor = leftModel }, Cmd.map LeftTutor cmd)
 
     RightTutor rightMsg ->
       let
-        (rightModel, rightCmd) =
+        (rightModel, cmd) =
             Tutor.update rightMsg model.rightTutor
       in
-        ({ model | rightTutor = rightModel }, Cmd.map RightTutor rightCmd)
+        ({ model | rightTutor = rightModel }, Cmd.map RightTutor cmd)
+
+    MonitorMsg monMsg ->
+      let
+        (mod, cmd) = Monitor.update monMsg model.monitor
+      in
+        ({ model | monitor = mod }, Cmd.map MonitorMsg cmd)
 
 
 view : Model -> Html Msg
 view model =
-  div []
+  div [ style [ ("display", "flex")
+              , ("position", "relative")
+              , ("height", "100%") ] ]
     [ Html.map LeftTutor (Tutor.view model.leftTutor)
     , Html.map RightTutor (Tutor.view model.rightTutor)
+    , div [ style [ ("position", "absolute")
+                  , ("top", "50%")
+                  , ("left", "0")
+                  , ("width", "100%") ] ]
+      [ Html.map MonitorMsg (Monitor.view model.monitor) ]
     ]
 
 subscriptions : Model -> Sub Msg
@@ -63,4 +81,5 @@ subscriptions model =
   Sub.batch
     [ Sub.map LeftTutor (Tutor.subscriptions model.leftTutor)
     , Sub.map RightTutor (Tutor.subscriptions model.rightTutor)
+    , Sub.map MonitorMsg (Monitor.subscriptions model.monitor)
     ]
