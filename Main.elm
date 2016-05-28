83cbcc46fd7be4c08984ec9f7ff73424d5b9fe48
phil <uniphil@gmail.com>
2016-05-28 13:51:56 -0400

    counter with full program thing

--- Main.elm
+++ Main.elm
@@ -1,23 +1,44 @@
-import Html exposing (Html, button, div, text)
+import Html exposing (..)
 import Html.App as Html
-import Html.Events exposing (onClick)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
 
 main =
-    Html.beginnerProgram { model = 0, view = view, update = update }
+  Html.program
+    { init = init
+    , view = view
+    , update = update
+    , subscriptions = subscriptions
+    }
+
+type alias Model =
+    { count: Int
+    }
+
+init : (Model, Cmd Msg)
+init =
+    (Model 0, Cmd.none)
+
 
 type Msg = Increment | Decrement
 
+update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
     case msg of
         Increment ->
-            model + 1
+            ({ model | count = model.count + 1 }, Cmd.none)
 
         Decrement ->
-            model - 1
+            ({ model | count = model.count - 1 }, Cmd.none)
 
+view : Model -> Html Msg
 view model =
     div []
         [ button [ onClick Decrement ] [ text "-" ]
-        , div [] [ text (toString model) ]
+        , div [] [ text (toString model.count) ]
         , button [ onClick Increment ] [ text "+" ]
         ]
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
